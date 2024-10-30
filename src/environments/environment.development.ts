6b78fc040cee1f9060d3095fbd6d38d4de82508d
Shai <benhemoshai@gmail.com>
2024-10-30 15:49:41 +0200

    added

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-   apiURL: "https://e-store-backend-8i6h.onrender.com"
-    //  apiURL: "http://localhost:3000"
+    production: false,
+    apiURL: "http://localhost:3000"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
+    production: true,
     apiURL: "https://e-store-backend-8i6h.onrender.com"
-     //apiURL: "http://localhost:3000"
 };
