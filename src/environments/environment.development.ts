966ac7b3ca36a0b4cd5f6e2f668ce5b89202012f
Shai <benhemoshai@gmail.com>
2024-11-28 12:38:09 +0200

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
    // apiURL: "https://e-store-c650f336290d.herokuapp.com"
-  //apiURL: "https://e-store-backend-8i6h.onrender.com"
- apiURL: "http://localhost:3000"
+  apiURL: "https://e-store-backend-8i6h.onrender.com"
+ //apiURL: "http://localhost:3000"
 };
